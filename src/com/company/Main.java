ba261833f8dc5fb102b4e04faf9b5c5441c2ae5f
softmartian <softmartian@gmail.com>
2015-12-24 21:11:22 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Java");
+        int[] m1 = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < m1.length; i++) {
+            m1[i] = -500 + random.nextInt(1000);
+        }
+        for (int i = 0; i < m1.length; i++) {
+            System.out.println(i+":"+m1[i]);
+        }
+            int[] m2 = new int[100];
+        int j = 0;
+        for (int i = 0; i < m2.length; i++) {
+            int a = i % 2;
+            if (m1[i] > 0 && a == 0) {
+                m2[j] = m1[i];
+                j++;
+            }
+        }
+        for (int i = 0; i < m2.length; i++) {
+            System.out.println(i+":"+m2[i]);
+        }
     }
 }
